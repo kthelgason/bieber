d78b0e2807488b8243ee835a54c207acd47ae8a3
Kári Tristan Helgason <kthelgason@gmail.com>
2015-02-21 15:10:53 +0000

    Abstracted db check into method that ensures the database exists

--- app.js
+++ app.js
@@ -21,34 +21,39 @@ function createDB(){
 
 function usage() {
     console.log("\t%s init [project name]", APP_NAME);
-    console.log("\t%s store [name] [url]", APP_NAME);
+    console.log("\t%s add [name] [url]", APP_NAME);
 }
 
-function addRecord(vals){
+function withDb(fn, args){
     if(!db){
         db = new sql.Database('.bieber', sql.OPEN_READWRITE, function(err,res){
             if(err){
                 console.log("Project not initialized.");
                 console.log("Run \"%s init [name]\" to get started.", APP_NAME);
             } else {
-                console.log(res);
-                console.log("Would you like to add a description now?");
-                prompt.get([{
-                    name: 'answer',
-                    description: 'y/n',
-                    type: 'string'
-                }], function(err, res){
-                    if(res.answer == 'y'){
-                        prompt.get(['description'], function(err, res){
-                            insertRecord(vals[0],vals[1], res.description);    
-                        });
-                        return;
-                    }
-                    insertRecord(vals[0],vals[1], "");    
-                });
+                fn.apply(null,args);
             }
         });
+    } else {
+        fn.apply(null,args);
     }
+}
+
+function addRecord(vals){
+    console.log("Would you like to add a description now?");
+    prompt.get([{
+        name: 'answer',
+        description: 'y/n',
+        type: 'string'
+    }], function(err, res){
+        if(res.answer == 'y'){
+            prompt.get(['description'], function(err, res){
+                insertRecord(vals[0],vals[1], res.description);    
+            });
+            return;
+        }
+        insertRecord(vals[0],vals[1], "");    
+    });
 }
 
 function insertRecord(name, url, desc){
@@ -64,8 +69,8 @@ function insertRecord(name, url, desc){
 function parseCommand(line) {
     if(line[0] == 'init'){
         createDB();
-    } else if(line[0] == 'store'){
-        addRecord(line.slice(1));
+    } else if(line[0] == 'add'){
+        withDb(addRecord, line.slice(1));
     } else {
         console.log("No command specified!");
         console.log("Usage:");
