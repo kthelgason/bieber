4309fffd695fd81c4ed35f1c8bb907cac76f6b0c
Kári Tristan Helgason <kthelgason@gmail.com>
2015-02-21 14:17:01 +0000

    Now creating a table for refs

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+/* Constants */
+var APP_NAME = 'bieber';
+
 
 var sql = require('sqlite3');
 var fs  = require('fs');
@@ -7,11 +10,15 @@ function createDB(){
     var db = new sql.Database('.bieber', function(err, ev){
         if(!err) console.log("Successfully initialized empty .bieber file");
     });
+    db.run("CREATE TABLE ref(id INTEGER PRIMARY KEY ASC," + 
+                            "name TEXT," + 
+                            "url TEXT," +
+                            "desc TEXT)");
     return db;
 }
 
 function usage() {
-    console.log("durp");
+    console.log("\t%s init [project name]", APP_NAME);
 }
 
 function parseCommand(line) {
